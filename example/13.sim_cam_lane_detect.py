adc4915715074a112b1a5f30164f123321dd4d0d
lee7198 <lee7198@naver.com>
2024-01-30 21:43:34 +0900

    line detact

--- example/13.sim_cam_lane_detect.py
+++ example/13.sim_cam_lane_detect.py
@@ -5,6 +5,7 @@
 
 import rospy
 from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import Float64
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
@@ -14,8 +15,14 @@ class Lane_sub:
     def __init__(self):
         rospy.init_node("lane_sub_node")
         rospy.Subscriber("/image_jpeg/compressed", CompressedImage, self.cam_CB)
+        self.speed_pub = rospy.Publisher("/commands/motor/speed", Float64, queue_size=1)
+        self.steer_pub = rospy.Publisher(
+            "/commands/servo/position", Float64, queue_size=1
+        )
         self.image_msg = CompressedImage()
         self.bridge = CvBridge()
+        self.speed_msg = Float64()
+        self.steer_msg = Float64()
 
     def cam_CB(self, msg: CompressedImage):
         img = self.bridge.compressed_imgmsg_to_cv2(msg)
@@ -46,15 +53,15 @@ class Lane_sub:
 
         # warped image
         src_point1 = [0, 420]
-        src_point2 = [260, 260]
-        src_point3 = [x - 260, 260]
+        src_point2 = [275, 260]
+        src_point3 = [x - 275, 260]
         src_point4 = [x, 420]
         src_point5 = np.float32([src_point1, src_point2, src_point3, src_point4])
 
         dst_point1 = [x // 8, 480]
         dst_point2 = [x // 8, 0]
-        dst_point3 = [x // 8 * 5, 0]
-        dst_point4 = [x // 8 * 5, 480]
+        dst_point3 = [x // 8 * 7, 0]
+        dst_point4 = [x // 8 * 7, 480]
         dst_point5 = np.float32([dst_point1, dst_point2, dst_point3, dst_point4])
 
         matrix = cv2.getPerspectiveTransform(src_point5, dst_point5)
@@ -67,6 +74,43 @@ class Lane_sub:
         bin_img = np.zeros_like(grayed_img)
         # image thresholding
         bin_img[(grayed_img > 50)] = 255
+
+        hist = np.sum(bin_img, axis=0)
+        l_hist = np.sum(hist[: x // 2])
+        r_hist = np.sum(hist[x // 2 :])
+
+        # calc center index
+        l_indices = np.where(l_hist > 20)[0]
+        r_indices = np.where(r_hist > 20)[0] + 320
+        indices = np.where(hist > 20)[0]
+
+        try:
+            if len(l_indices) != 0 and len(r_indices) != 0:
+                center_index = (indices[0] + indices[-1]) // 2
+                print("both line")
+            elif len(l_indices) != 0 and len(r_indices) == 0:
+                center_index = (l_indices[0] + l_indices[-1]) // 2
+                print("left line")
+            elif len(l_indices) == 0 and len(r_indices) != 0:
+                center_index = (r_indices[0] + r_indices[-1]) // 2
+                print("right line")
+        except:
+            center_index = x // 2
+            print("no line")
+
+        print("center index: ", center_index)
+        # calc steer value
+        standard_line = x // 2
+        degree_per_pixel = 1 / x
+        # steer = 0.5 + (((center_index - standard_line) * 0.02 / 3.2) / 2)
+        steer = (center_index + standard_line) * degree_per_pixel
+        steer = steer + 0.5
+
+        print("steer: ", Float64(steer))
+        self.steer_msg.data = steer
+        self.speed_msg.data = 1000
+        self.speed_pub.publish(self.speed_msg)
+        self.steer_pub.publish(self.steer_msg)
 
         # cv2.imshow("yellow_img_range", yellow_img_range)
         # cv2.imshow("white_img_range", white_img_range)
